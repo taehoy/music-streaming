63fdca7221f5d14b21abdac1221b0d92493402e6
taehoy <rethnoly@gmail.com>
2025-06-26 08:40:38 +0900

    feat: 홈화면 실시간 차트 구현

--- src/main/java/com/music/controller/HomeController.java
+++ src/main/java/com/music/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.music.controller;
 
+import com.music.domain.response.RealtimeChartResponse;
+import com.music.service.ChartService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final ChartService chartService;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<RealtimeChartResponse> realtimeChart = chartService.getRealtimeChart(10);
+
+        model.addAttribute("chart", realtimeChart);
         return "index";
     }
 }
