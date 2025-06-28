a96ecdcd96c6b244f493ad106d75827c2fa4b9d0
taehoy <rethnoly@gmail.com>
2025-06-28 09:00:21 +0900

    feat: 플레이리스트 상세페이지 추가

--- src/main/java/com/music/controller/PlaylistController.java
+++ src/main/java/com/music/controller/PlaylistController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,5 +15,12 @@ public class PlaylistController {
     public String playlistDetailPage(@PathVariable Long id, Model model) {
         model.addAttribute("playlistId", id);
         return "playlist/detail";
+    }
+
+    @GetMapping("/me/playlists")
+    public String myPlaylistsPage(@RequestParam(required = false, defaultValue = "1") Long userId, //Todo : 플레이리스트 userId 추후 변경(로그인 구현 시)
+                                  Model model) {
+        model.addAttribute("userId", userId);
+        return "playlist/manage";
     }
 }
